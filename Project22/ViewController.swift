312fd4f2191b65f80573008133b81634b95ddaec
retessam7 <retessam7@gmail.com>
2025-02-14 15:02:48 +0100

    CLBeaconRegion

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -38,12 +38,59 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                     // Ranging is the ability to tell roughly how far something else is away from our device
                     if CLLocationManager.isRangingAvailable() {
-                        // do stuff
+                        startScanning()
                     }
                 }
         default:
             break
             }
         }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReading.text = "RIGHT HERE"
+            
+            // CLProximity as an enum that might change in the future
+            @unknown default:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            }
+        }
+    }
 }
 
+// UUID: You're in a Acme Hardware Supplies store.
+// Major: You're in the Glasgow branch.
+// Minor: You're in the shoe department on the third floor.
